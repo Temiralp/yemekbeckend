6320b9a131b8b658d5b552d25b5bcf1a03766adf
omer-marin <erturkomer256@gmail.com@users.noreply.github.com>
2025-03-25 11:11:09 +0300

    backend rest api enpoint.txt

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,5 +19,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// Firebase fonksiyonlarını dışa aktar
 export { signInWithPhoneNumber, RecaptchaVerifier };
